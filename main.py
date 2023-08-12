c80b7a9812c692de37679f94a2f007c147d77b0b
Arnab Pani <arnab.pani1@gmail.com>
2023-08-12 13:58:52 +0530

    DataIngestion Completed

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import pymongo
+import os, sys
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
+from sensor.components import data_ingestion
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Database Name
-dataBase = client["neurolabDB"]
+if __name__ == "__main__":
+     try:
+          #get_collection_as_dataframe(database_name = "aps", collection_name = "sensor")
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config= training_pipeline_config)
+          print(data_ingestion_config.to_dict())
 
-# Collection  Name
-collection = dataBase['Products']
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          print(e)
---
+++ sensor/__init__.py
@@ -0,0 +1,3 @@
+from dotenv import load_dotenv
+print(f"Loading Environment variables from .env file")
+load_dotenv()
